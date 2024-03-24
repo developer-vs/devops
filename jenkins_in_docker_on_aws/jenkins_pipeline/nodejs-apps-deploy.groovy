22063710d2dc176437f57841140217cedac521c8
Vlad <vladimirgdsr@gmail.com>
2024-03-24 00:32:04 -0700

    code refactoring

--- jenkins_in_docker_on_aws/jenkins_pipeline/nodejs-apps-deploy.groovy
+++ jenkins_in_docker_on_aws/jenkins_pipeline/nodejs-apps-deploy.groovy
@@ -4,9 +4,9 @@ pipeline {
     environment {
         GIT_REPO =          'git@github.com:developer-vs/devops.git'
         GIT_CREDENTIALS =   'Github'
-        TF_DIRECTORY_S3 =   'project-setup-files/terraform_s3'
-        TF_DIRECTORY =      'project-setup-files/terraform'
-        ANSIBLE_DIRECTORY = 'project-setup-files/ansible'
+        TF_DIRECTORY_S3 =   'jenkins_in_docker_on_aws/project-setup-files/terraform_s3'
+        TF_DIRECTORY =      'jenkins_in_docker_on_aws/project-setup-files/terraform'
+        ANSIBLE_DIRECTORY = 'jenkins_in_docker_on_aws/project-setup-files/ansible'
     }
 
     stages {
@@ -70,26 +70,6 @@ pipeline {
                 dir("${ANSIBLE_DIRECTORY}") {
                     script {
                         sh 'ansible-playbook deploy-jenkins.yml'                        
-                    }
-                }
-            }
-        }
-        
-        stage('Copy Jenkins Files to EC2') {
-            steps {
-                dir("${ANSIBLE_DIRECTORY}") {
-                    script {
-                        sh './copy_jenkins_files_to_ec2.sh'                        
-                    }
-                }
-            }
-        }
-        
-        stage('Restart Jenkins') {
-            steps {
-                dir("${ANSIBLE_DIRECTORY}") {
-                    script {
-                        sh 'ansible-playbook restart_jenkins.yml'                        
                     }
                 }
             }
--- jenkins_in_docker_on_aws/project-setup-files/ansible/copy_jenkins_files_to_ec2.sh
+++ jenkins_in_docker_on_aws/project-setup-files/ansible/copy_jenkins_files_to_ec2.sh
@@ -6,42 +6,45 @@ ssh_key="../terraform/jenkins-server.pem"
 # Set the instance IP address
 instance_ip=$(sed 's/"//g' instance_ip.txt)
 
-# Rsync command to copy files
+# Define the jenkins_home directory
+jenkins_home_dir="../../../../../../../docker/jenkins_home/"
+
+# Rsync command to copy jobs directory
 rsync -avz -e "ssh -i $ssh_key" \
-    ../../../../../../docker/jenkins_home/jobs/ \
+    "${jenkins_home_dir}jobs/" \
     "ubuntu@$instance_ip:/home/ubuntu/jenkins_data/jobs"
     
 # Check if Rsync was successful
 if [ $? -eq 0 ]; then
-    echo "File copied successfully to remote host."
+    echo "Jobs directory copied successfully to remote host."
 else
-    echo "Failed to copy file to remote host."
+    echo "Failed to copy jobs directory to remote host."
     exit 1
 fi
     
-# Rsync command to copy files
+# Rsync command to copy credentials.xml file
 rsync -avz -e "ssh -i $ssh_key" \
-    ../../../../../../docker/jenkins_home/credentials.xml \
+    "${jenkins_home_dir}credentials.xml" \
     "ubuntu@$instance_ip:/home/ubuntu/jenkins_data/"
 
 # Check if Rsync was successful
 if [ $? -eq 0 ]; then
-    echo "File copied successfully to remote host."
+    echo "credentials.xml file copied successfully to remote host."
 else
-    echo "Failed to copy file to remote host."
+    echo "Failed to copy credentials.xml file to remote host."
     exit 1
 fi
 
-# Rsync command to copy id_ed25519 file only
+# Rsync command to copy id_ed25519 file
 rsync -avz -e "ssh -i $ssh_key" \
-    ../../../../../../docker/jenkins_home/.ssh/id_ed25519 \
+    "${jenkins_home_dir}.ssh/id_ed25519" \
     "ubuntu@$instance_ip:/home/ubuntu/jenkins_data/.ssh/"
 
 # Check if Rsync was successful
 if [ $? -eq 0 ]; then
-    echo "File copied successfully to remote host."
+    echo "id_ed25519 file copied successfully to remote host."
 else
-    echo "Failed to copy file to remote host."
+    echo "Failed to copy id_ed25519 file to remote host."
     exit 1
 fi
 
