b32fbdf22ef40bb7efc88698b7f340c87a8b4e62
Vlad <vladimirgdsr@gmail.com>
2024-04-01 15:02:50 -0700

    pacman in k3s cluster on aws

---
+++ projects/k3s_cluster_aws/cluster_init/terraform/worker_node_config/variables.tf
@@ -0,0 +1,6 @@
+
+variable "k3s_cluster_token" {
+  description = "Shared token for K3s cluster node registration"
+  type        = string
+  default     = "u2Qw5PbXC887MMv85LeG" # Replace with your actual token
+}
