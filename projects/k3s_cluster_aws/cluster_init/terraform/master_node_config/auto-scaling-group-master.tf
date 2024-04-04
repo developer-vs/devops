b44e44fbea92ba28c5187ddc30cf7138ba0ac17c
Vlad <vladimirgdsr@gmail.com>
2024-04-03 17:41:34 -0700

    code refactoring

--- projects/k3s_cluster_aws/cluster_init/terraform/master_node_config/auto-scaling-group-master.tf
+++ projects/k3s_cluster_aws/cluster_init/terraform/master_node_config/auto-scaling-group-master.tf
@@ -1,4 +1,4 @@
-resource "aws_autoscaling_group" "k3s_master_asg" {
+resource "aws_instance" "k3s_master_asg" {
   launch_template {
     id      = aws_launch_template.k3s_master.id
     version = "$Latest"
